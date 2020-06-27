a1ca27abcbd6e5dc2876b19858db119dad07953d
Rodrigo Mello <rodrigojcmello@gmail.com>
2020-06-27 12:56:33 -0300

    eslint config

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,19 +1,27 @@
-import React, { FC, ReactNode } from 'react'
-import sButton from './button.module.scss'
-import sMain from '../main.module.scss'
+import React, { FC, ReactNode } from 'react';
+import sButton from './button.module.scss';
+import sMain from '../main.module.scss';
 
 interface ButtonProps {
-    children: ReactNode;
+  children: ReactNode;
 
-    /**
-     * Tipo do botão, padrão HTML5
-     * @default "button"
-     **/
-    type?: 'submit' | 'reset' | 'button';
+  /**
+   * Tipo do botão, padrão HTML5
+   * @default "submit"
+   * */
+  type?: 'submit' | 'reset' | 'button';
 }
 
-const Button: FC<ButtonProps> = ({children, type}) => {
-    return <button className={[sButton['button'], sMain['p4']].join(' ')} type={type || 'button'}>{children}</button>
-}
+const Button: FC<ButtonProps> = ({ children, type }) => {
+  return (
+    <button
+      className={[sButton.button, sMain.p4].join(' ')}
+      /* eslint-disable-next-line react/button-has-type */
+      type={type || 'submit'}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
--- components/index.ts
+++ components/index.ts
@@ -1,20 +1,26 @@
-export { default as Button } from './Button'
-export { default as Text } from './Text'
+export { default as Button } from './Button';
+export { default as Text } from './Text';
 
 const themeColors = {
-    itaucard: {
-        colorPrimary: 'orange',
-        colorPrimaryContrast: 'white'
-    },
-    credicard: {
-        colorPrimary: 'blue',
-        colorPrimaryContrast: 'yellow'
-    }
-}
+  itaucard: {
+    colorPrimary: 'orange',
+    colorPrimaryContrast: 'white',
+  },
+  credicard: {
+    colorPrimary: 'blue',
+    colorPrimaryContrast: 'yellow',
+  },
+};
 
 type Theme = 'itaucard' | 'credicard';
 
 export function setTheme(theme: Theme, element: HTMLElement) {
-    element.style.setProperty('--itau-color-primary', themeColors[theme].colorPrimary);
-    element.style.setProperty('--itau-color-primary-contrast', themeColors[theme].colorPrimaryContrast);
+  element.style.setProperty(
+    '--itau-color-primary',
+    themeColors[theme].colorPrimary
+  );
+  element.style.setProperty(
+    '--itau-color-primary-contrast',
+    themeColors[theme].colorPrimaryContrast
+  );
 }
